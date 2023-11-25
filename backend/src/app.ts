f41d9eb0461864b25114777bb580c8b81cfb4b69
Massimiliano Schindele <massimilianoschindele@gmail.com>
2023-11-25 14:25:52 +0100

    Add .env support

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,11 @@
+import path from 'path';
+import dotenv from 'dotenv';
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const envPath = path.resolve(__dirname, '../..', '.env');
+dotenv.config({ path: envPath });
+const port = process.env.EXPO_PUBLIC_PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('🚀');
--- common/constants.ts
+++ common/constants.ts
@@ -1,1 +1,10 @@
-export const SOCKET_ENDPOINT = 'http://192.168.178.21:3000';
+import path from 'path';
+import dotenv from 'dotenv';
+
+const envPath = path.resolve(__dirname, '..', '.env');
+dotenv.config({ path: envPath });
+
+const IP = process.env.EXPO_PUBLIC_IP || '192.168.178.21';
+const PORT = process.env.EXPO_PUBLIC_PORT || 3000;
+
+export const SOCKET_ENDPOINT = `http://${IP}:${PORT}`;
